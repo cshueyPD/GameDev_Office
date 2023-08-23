686b0860ceeaa9be5f2c622edf4c8afc544823fd
AdamAntium07 <j.adam.shank@gmail.com>
2023-08-23 12:14:35 -0400

    Changed so that objects spawn in zones

--- Assets/PD_Spawner.cs
+++ Assets/PD_Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PD_Spawner : MonoBehaviour
 {
    public GameObject Cheezit;
-   public int numCheezit =5;  //Number of CheezIts to spawn
+   public int numCheezit =1;  //Number of CheezIts to spawn
 
 void Start()
 {
@@ -13,8 +13,8 @@ int i=0;
 
     while (i < numCheezit)
     {
-      int spawnPointX = Random.Range(-14,17);  //Pick Random X Position
-      int spawnPointY = Random.Range(7,24);     //Pick Random Y Position
+      int spawnPointX = Random.Range(-14,-4);  //Pick Random X Position
+      int spawnPointY = Random.Range(7,16);     //Pick Random Y Position
       Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
 
    if(FindCollisions(spawnPosition)<1)   
@@ -23,6 +23,77 @@ int i=0;
    i++;
    }
     }
+
+i=0;
+while (i < numCheezit)
+    {
+      int spawnPointX = Random.Range(-4,6);  //Pick Random X Position
+      int spawnPointY = Random.Range(7,16);     //Pick Random Y Position
+      Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
+
+   if(FindCollisions(spawnPosition)<1)   
+   {
+   Instantiate (Cheezit,spawnPosition,Quaternion.identity);  // If there are no collisions, spawn CheezIt
+   i++;
+   }
+    }
+
+i=0;
+while (i < numCheezit)
+    {
+      int spawnPointX = Random.Range(6,17);  //Pick Random X Position
+      int spawnPointY = Random.Range(7,16);     //Pick Random Y Position
+      Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
+
+   if(FindCollisions(spawnPosition)<1)   
+   {
+   Instantiate (Cheezit,spawnPosition,Quaternion.identity);  // If there are no collisions, spawn CheezIt
+   i++;
+   }
+    }
+
+i=0;
+while (i < numCheezit)
+    {
+      int spawnPointX = Random.Range(-14,-4);  //Pick Random X Position
+      int spawnPointY = Random.Range(16,24);     //Pick Random Y Position
+      Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
+
+   if(FindCollisions(spawnPosition)<1)   
+   {
+   Instantiate (Cheezit,spawnPosition,Quaternion.identity);  // If there are no collisions, spawn CheezIt
+   i++;
+   }
+    }
+
+i=0;
+while (i < numCheezit)
+    {
+      int spawnPointX = Random.Range(-4,6);  //Pick Random X Position
+      int spawnPointY = Random.Range(16,24);     //Pick Random Y Position
+      Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
+
+   if(FindCollisions(spawnPosition)<1)   
+   {
+   Instantiate (Cheezit,spawnPosition,Quaternion.identity);  // If there are no collisions, spawn CheezIt
+   i++;
+   }
+    }
+
+i=0;
+while (i < numCheezit)
+    {
+      int spawnPointX = Random.Range(6,17);  //Pick Random X Position
+      int spawnPointY = Random.Range(16,24);     //Pick Random Y Position
+      Vector2 spawnPosition = new Vector2(spawnPointX,spawnPointY);
+
+   if(FindCollisions(spawnPosition)<1)   
+   {
+   Instantiate (Cheezit,spawnPosition,Quaternion.identity);  // If there are no collisions, spawn CheezIt
+   i++;
+   }
+    }
+
 }
 
 
@@ -32,7 +103,7 @@ int i=0;
 private int FindCollisions(Vector2 pos)  //Check for collisions
 {
 Collider2D[] hits = Physics2D.OverlapCircleAll(pos, 1f);
-Debug.Log(hits.Length);
+//Debug.Log(hits.Length);
 return hits.Length;
 
 
