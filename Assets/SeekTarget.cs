a4754c95d2f7a19103010b1aa3fa5ab140abf9a1
AdamAntium07 <j.adam.shank@gmail.com>
2023-09-20 09:23:27 -0400

    added enemies that seek a target

--- Assets/SeekTarget.cs
+++ Assets/SeekTarget.cs
@@ -7,6 +7,7 @@ public class SeekTarget : MonoBehaviour
 
     [SerializeField] private GameObject target;
     [SerializeField] private float speed = 1.5f; 
+    [SerializeField] private float distanceToPlayer = 5f; 
 
     private float distance;
 
@@ -19,7 +20,7 @@ public class SeekTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(target.transform.position,transform.position)<5f)
+        if(Vector2.Distance(target.transform.position,transform.position)<distanceToPlayer)
         {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
         transform.up = target.transform.position - transform.position;
