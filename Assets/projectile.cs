cbf52a174aa70a77b5c54af5a379187712330dac
AdamAntium07 <j.adam.shank@gmail.com>
2023-09-20 09:23:56 -0400

    Added seeking enemies

---
+++ Assets/projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    
+public float projectileSpeed = 10f;
+
+Rigidbody2D rb;
+
+private void Start()
+{
+
+rb = GetComponent<Rigidbody2D>();
+Vector2 force = transform.right;
+
+
+}
+
+}
